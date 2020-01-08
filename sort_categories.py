615bf3296b2f065d3ce1ea0b96107ab79767f492
Mykola-Z <36878794+Mykola-Z@users.noreply.github.com>
2020-01-08 17:07:07 +1300

    Create sort_categories.py
    Sorting task

---
+++ sort_categories.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 14:55:11 2020
+
+@author: mzly903
+"""
+
+file_se= open("input.txt", 'r').readlines() 
+
+dict = {}
+
+number_of_catigories= int(input('How many categories you want to find? \n or just press enter if it\'s 2: ') or '2')
+default_categories = ['NUMBERS', 'ANIMALS']
+i= 0
+for i in range(number_of_catigories):
+    
+    category = input('Please input a category. \n if it\'s {} just press enter: '.format(default_categories[i])) or default_categories[i]
+         
+    dict[category] = []
+ 
+for i in range(0,len(file_se)):
+    
+    file_se[i]= file_se[i].replace("\n", "") # a small fuck up
+
+    if str(file_se[i]) in dict:
+        y= i
+        
+    elif file_se[i] not in dict:
+        dict[file_se[y]].append(file_se[i])
+
+
+for i in dict : 
+    print(i+ ': \n', dict[i]) 
